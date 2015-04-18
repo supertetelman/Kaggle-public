f0b116e3d75c8f7f3b46cc2913aec1f56ec2acd7
Adam Tetelman <adam@deep.is>
2015-04-17 22:44:40 -0400

    More tweaks to output

--- otto/otto.m
+++ otto/otto.m
@@ -8,21 +8,23 @@ kmeans = 1;
 logistic = 1;
 
 classifiers = 9;
-lambda = 0;
-epsilon = .9;
-k_iters = 400;
-log_iters = 500;
-min_clusters = 9;
+lambda = 100;
+epsilon = .8;
+k_iters = 100;
+log_iters = 1000;
+min_clusters = 4;
 
 
 %Read data in 
 train = csvread('train.csv');
 if debug == 0
+	disp('Using real dataset')
 	[ mytrain cv mytest ] = makedata(train, .6, .2, .2, true);
 else
 	[ mytrain cv mytest ] = makedata(train, .2, .1, .1, true);
 end
 if debug_sol == 0
+	disp('Using real test set')
 	test = csvread('test.csv');
 else
 	test = mytest;
@@ -54,10 +56,17 @@ if (logistic && kmeans)
 	solution = log_predict(theta,[ones(m,1) test(:,2:end-1)], classifiers);
 	csvwrite('solution.results.logistic.csv', [test(:,1) solution ])
 end
+
+all_solution = zeros(m,classifiers+1);
+all_solution(:,1) = test(:,1);
+for i=1:classifiers
+	all_solution(:,i) = (solution == i);
+end
+%TODO Add header
+csvwrite('all.solution.results.logistic.csv', [all_solution ])
+
 %TODO:
 %tune a accuracy for the clusters
-%Right function that takes centers and data and returns which centers are closest
-%determin accurace vi cv rather than train
 %
 %Pipe each cluster into a seperate logistic regression
 %tune lambda
