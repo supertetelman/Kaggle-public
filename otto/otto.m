3a1a702a90449c2dcc9f27c5ebf6f0e2877575a8
Adam Tetelman <adam@deep.is>
2015-04-17 22:18:25 -0400

    tweak otto to run all combos

--- otto/otto.m
+++ otto/otto.m
@@ -1,17 +1,17 @@
 %load reqs
 pkg load statistics
 
-debug = 1;
+debug = 0;
 debug_sol = 1;
 
-kmeans = 0;
+kmeans = 1;
 logistic = 1;
 
 classifiers = 9;
 lambda = 0;
-epsilon = .8;
+epsilon = .9;
 k_iters = 400;
-log_iters = 5;
+log_iters = 500;
 min_clusters = 9;
 
 
@@ -31,6 +31,14 @@ end
 [m n] = size(test);
 unknown = zeros(m);
 
+if logistic
+	map = 0;
+	[ theta predict_train predict_cv predict_test ] = all_logistic(mytrain, cv, mytest, lambda, log_iters, map, classifiers);
+	[m n] = size(test);
+	solution = log_predict(theta,[ones(m,1) test(:,2:end-1)], classifiers);
+	csvwrite('solution.results.logistic.csv', [test(:,1) solution ])
+end
+
 if kmeans
 	[ map centers predict_train predict_cv predict_test] = all_kmeans(mytrain, cv, mytest, epsilon, min_clusters, k_iters);
 
@@ -40,8 +48,7 @@ if kmeans
 	csvwrite([num2str(length(centers)) '.solution.results.kmeans.csv'],[ k_solution ])
 end
 
-if logistic
-	map = 0;
+if logistic and kmeans
 	[ theta predict_train predict_cv predict_test ] = all_logistic(mytrain, cv, mytest, lambda, log_iters, map, classifiers);
 	[m n] = size(test);
 	solution = log_predict(theta,[ones(m,1) test(:,2:end-1)], classifiers);
