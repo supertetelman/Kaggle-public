6ffed357f0d8c959e06f68fb6ad596deac66b262
Adam Tetelman <adam@deep.is>
2015-04-20 09:58:11 -0400

    fixe bug in map assignement

--- otto/runkmeans.m
+++ otto/runkmeans.m
@@ -2,7 +2,7 @@ function [ idx centers map ] = runkmeans(data, y, clusters, iters)
 
 %Train kmeans centers
 [idx, centers] = kmeans(data,clusters, 'MaxIter',iters);
-map=zeros(clusters);
+map=zeros(clusters,1);
 
 for i=1:clusters
         map(i) = mode(y(idx == i)); %Create map the most popular classnum of each cluster to the clusternum
